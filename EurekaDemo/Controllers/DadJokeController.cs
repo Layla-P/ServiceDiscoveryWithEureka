ad045c71daa4760adc222d9e94e2f2ed4fd1a137
layla-p <layla.porter@gmail.com>
2021-08-17 11:04:57 +0100

    with steeltoe and eureka connectivity in place

---
+++ EurekaDemo/Controllers/DadJokeController.cs
@@ -0,0 +1,32 @@
+﻿using EurekaDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EurekaDemo.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class DadJokeController : ControllerBase
+	{
+		
+
+		private readonly ILogger<DadJokeController> _logger;
+		private readonly DadJokeService _dadJokeService;
+
+		public DadJokeController(ILogger<DadJokeController> logger, DadJokeService dadJokeService)
+		{
+			_logger = logger;
+			_dadJokeService = dadJokeService;
+		}
+
+		[HttpGet]
+		public async Task<string> Get()
+		{
+			return await _dadJokeService.GetJokeAsync();
+		}
+	}
+}
